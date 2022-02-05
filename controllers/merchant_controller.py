e0872cdf7844344e9e6d482eee831302f1c1e498
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-05 13:34:40 +0000

    creates new merchant

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -1,10 +1,22 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, redirect, render_template, request
 from models.merchant import Merchant
 import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint('merchants', __name__)
 
+# @merchants_blueprint.route('/merchants')
+# def merchants():
+#     merchants = merchant_repository.select_all()
+#     return render_template('merchants/index.html', merchants=merchants)
+
 @merchants_blueprint.route('/merchants')
-def merchants():
+def new_merchant_current_merchants():
     merchants = merchant_repository.select_all()
     return render_template('merchants/index.html', merchants=merchants)
+
+@merchants_blueprint.route('/merchants', methods=['POST'])
+def create_merchant():
+    name = request.form['name']
+    merchant = Merchant(name)
+    merchant_repository.save(merchant)
+    return redirect('/merchants')
