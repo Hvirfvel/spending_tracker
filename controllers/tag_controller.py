f49e5805fe8de9a33adfcf4c468a0cb5015aa17e
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-05 20:51:01 +0000

    creates new tag

--- controllers/tag_controller.py
+++ controllers/tag_controller.py
@@ -1,10 +1,18 @@
+from crypt import methods
 from flask import Flask, Blueprint, render_template, request, redirect
 from models.tag import Tag
 import repositories.tag_repository as tag_repository
 
-tags_blueprint = Blueprint('tags' ,__name__)
+tags_blueprint = Blueprint('tags', __name__)
 
 @tags_blueprint.route('/tags')
 def index():
     tags = tag_repository.select_all()
     return render_template('tags/index.html', tags=tags)
+
+@tags_blueprint.route('/tags', methods=['POST'])
+def create_tag():
+    name = request.form['name']
+    tag = Tag(name)
+    tag_repository.save(tag)
+    return redirect('/tags')
