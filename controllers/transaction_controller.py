e36d9c2af254bd0732bf68035b6718b401143851
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-06 12:12:31 +0000

    creates new transaction, connecting to existing merchant and tag

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -1,6 +1,9 @@
-from flask import Flask, Blueprint, render_template
+from crypt import methods
+from flask import Flask, Blueprint, redirect, render_template, request
 from models.transaction import Transaction
 import repositories.transaction_repository as transaction_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
 
 transactions_blueprint = Blueprint('transactions', __name__)
 
@@ -8,3 +11,22 @@ transactions_blueprint = Blueprint('transactions', __name__)
 def index():
     transactions = transaction_repository.select_all()
     return render_template('transactions/index.html', transactions=transactions)
+
+@transactions_blueprint.route('/transactions/new')
+def new_transaction():
+    merchants = merchant_repository.select_all()
+    tags = tag_repository.select_all()
+    return render_template('transactions/new.html', merchants=merchants, tags=tags)
+
+@transactions_blueprint.route('/transactions/new', methods=['POST'])
+def create_transaction():
+    amount = request.form['amount']
+    date = request.form['date']
+    merchant_id = request.form['merchant_id']
+    tag_id = request.form['tag_id']
+    merchant = merchant_repository.select(merchant_id)
+    tag = tag_repository.select(tag_id)
+
+    transaction = Transaction(amount, date, merchant, tag)
+    transaction_repository.save(transaction)
+    return redirect('/transactions')
