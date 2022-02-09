0ff55e5479995f00673da1c8b57b3fce31cb5b51
Caroline Hvirfvel <carolinehvirfvel@Carolines-MacBook-Pro.local>
2022-02-09 22:23:16 +0000

    controller names to plural

--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -1,6 +1,5 @@
 from db.run_sql import run_sql
 from models.transaction import Transaction
-import repositories.transaction_repository as transaction_repository
 import repositories.merchant_repository as merchant_repository
 import repositories.tag_repository as tag_repository
 
